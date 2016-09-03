123d460de497d47574941e1fa202c38c992fd3d9
kdsmith18542 <kdsmith18542@email.phoenix.edu>
2016-09-03 10:49:57 -0500

    Added Seed Nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    	{"54.68.198.221", "54.68.198.221"},	
+    	{"198.245.50.37", "192.99.32.58"},	
 };
 
 void ThreadDNSAddressSeed(void* parg)
